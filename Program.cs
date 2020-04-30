205e22df9a00175c65818e2cf111036985c48630
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-04-30 01:13:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.voz();
+        }
+    }
+}    
+// Lo traté de hacer como lo hizo ud pero no me salió n.n
+// using System;
+
+// namespace elevator
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Menu menu = new Menu();
+//             menu.Display();
+//         }
+//     }
+// }
